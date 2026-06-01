c559c990382cca6a4c6c3ccfb1306003cdbee3c7
Miguel Ponce <miguelxponce20@gmail.com>
2026-06-01 10:59:13 -0400

    Fix: Rutas arregladas con los controladores. (refs Ponce)

--- routes/lineasRoutes.js
+++ routes/lineasRoutes.js
@@ -1,11 +1,96 @@
 import express from 'express';
-import { getLineas, crearLinea, actualizarLinea } from '../controllers/lineasControllers.js';
+import { body, param } from 'express-validator';
+import validarCampos from '../middlewares/validarCampos.js';
 import checkAuth from '../middlewares/authMiddleware.js';
+
+import {
+  createLinea,
+  listLineas,
+  getLineaById,
+  updateLinea,
+  deleteLinea
+} from '../controllers/lineaInvestigacionController.js';
 
 const router = express.Router();
 
-router.get('/', getLineas);
-router.post('/', checkAuth, crearLinea);
-router.put('/:id', checkAuth, actualizarLinea);
+router.get('/', listLineas);
+
+router.get(
+  '/:id',
+  [
+    param('id')
+      .isInt()
+      .withMessage('El ID debe ser numérico'),
+    validarCampos
+  ],
+  getLineaById
+);
+
+router.post(
+  '/',
+  checkAuth,
+  [
+    body('area_id')
+      .notEmpty()
+      .withMessage('El area_id es obligatorio')
+      .isInt()
+      .withMessage('El area_id debe ser numérico'),
+
+    body('nombre')
+      .notEmpty()
+      .withMessage('El nombre es obligatorio')
+      .isLength({ min: 3 })
+      .withMessage('El nombre debe tener al menos 3 caracteres'),
+
+    body('tipo')
+      .notEmpty()
+      .withMessage('El tipo es obligatorio')
+      .isIn(['Matriz', 'Asociada'])
+      .withMessage('El tipo debe ser Matriz o Asociada'),
+
+    validarCampos
+  ],
+  createLinea
+);
+
+router.put(
+  '/:id',
+  checkAuth,
+  [
+    param('id')
+      .isInt()
+      .withMessage('El ID debe ser numérico'),
+
+    body('area_id')
+      .optional()
+      .isInt()
+      .withMessage('El area_id debe ser numérico'),
+
+    body('nombre')
+      .optional()
+      .isLength({ min: 3 })
+      .withMessage('El nombre debe tener al menos 3 caracteres'),
+
+    body('tipo')
+      .optional()
+      .isIn(['Matriz', 'Asociada'])
+      .withMessage('El tipo debe ser Matriz o Asociada'),
+
+    validarCampos
+  ],
+  updateLinea
+);
+
+router.delete(
+  '/:id',
+  checkAuth,
+  [
+    param('id')
+      .isInt()
+      .withMessage('El ID debe ser numérico'),
+    validarCampos
+  ],
+  deleteLinea
+);
 
 export default router;
--- routes/programasRoutes.js
+++ routes/programasRoutes.js
@@ -1,11 +1,74 @@
 import express from 'express';
-import { getProgramas, crearPrograma, actualizarPrograma } from '../controllers/programasControllers.js';
+import { body, param } from 'express-validator';
+
+import {
+    getProgramas,
+    getProgramaById,
+    crearPrograma,
+    actualizarPrograma,
+    eliminarPrograma
+} from '../controllers/programasControllers.js';
+
 import checkAuth from '../middlewares/authMiddleware.js';
+import validarCampos from '../middlewares/validarCampos.js';
 
 const router = express.Router();
 
 router.get('/', getProgramas);
-router.post('/', checkAuth, crearPrograma);
-router.put('/:id', checkAuth, actualizarPrograma);
+
+router.get(
+    '/:id',
+    [
+        param('id')
+            .isInt()
+            .withMessage('El ID debe ser numérico'),
+        validarCampos
+    ],
+    getProgramaById
+);
+
+router.post(
+    '/',
+    checkAuth,
+    [
+        body('nombre')
+            .notEmpty()
+            .withMessage('El nombre es obligatorio')
+            .isLength({ min: 3 })
+            .withMessage('El nombre debe tener al menos 3 caracteres'),
+        validarCampos
+    ],
+    crearPrograma
+);
+
+router.put(
+    '/:id',
+    checkAuth,
+    [
+        param('id')
+            .isInt()
+            .withMessage('El ID debe ser numérico'),
+
+        body('nombre')
+            .optional()
+            .isLength({ min: 3 })
+            .withMessage('El nombre debe tener al menos 3 caracteres'),
+
+        validarCampos
+    ],
+    actualizarPrograma
+);
+
+router.delete(
+    '/:id',
+    checkAuth,
+    [
+        param('id')
+            .isInt()
+            .withMessage('El ID debe ser numérico'),
+        validarCampos
+    ],
+    eliminarPrograma
+);
 
 export default router;
